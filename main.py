c54511d4c0fdf64d98679fb6da7b599f95157b35
Elakkiyan <elakkiyan2006@gmail.com>
2023-01-10 13:43:04 -0500

    Added rolling, converting to binary, and retrieve letter. ONCE. </3

--- main.py
+++ main.py
@@ -23,28 +23,35 @@ def roll_and_binary_converter():
     rolls = []
     roll_and_binary_converter.rolls = []
     input("Press enter to roll: ")
-    for _ in range(8):
+    for _ in range(5):
         rolls.append(randint(1, 6))
     print(rolls)
     for number in range(len(rolls)):
-        print(number)
         if rolls[number] % 2 == 0:
             rolls[number] = 1
         else:
             rolls[number] = 0
         roll_and_binary_converter.rolls.append(rolls[number])
-    print(rolls)
+    print(roll_and_binary_converter.rolls)
+
 
 def binary_to_letter():
-    binary_data = ""
+    global letters
+    binary_data = "011"
     alphabet = list(string.ascii_lowercase)
-    for _ in roll_and_binary_converter.rolls:
-        binary_data = binary_data + str(_)
+    print(roll_and_binary_converter.rolls)
+    for number in roll_and_binary_converter.rolls:
+        binary_data = binary_data + str(number)
     # binary_data = int(binary_data)
-    binary_data = int(binary_data)
-    letter = chr(binary_data)
-    if letter not in alphabet:
-        while True:
+    print(binary_data)
+
+    while True:
+        try:
+            letter = chr(int(binary_data, 2))
+            print(letter)
+        except ValueError:
+            roll_and_binary_converter()
+        if letter not in alphabet:
             reroll = input("Your roll is invalid. Would you like to reroll? (Y/N) ").title()
             if reroll == "Y":
                 roll_and_binary_converter()
@@ -53,10 +60,11 @@ def binary_to_letter():
                 exit()
             else:
                 continue
-    else:
-        print(letter)
+        else:
+            roll_and_binary_converter()
 
-    print(letter)
+
+
 def main():
     want_to_play()
     roll_and_binary_converter()

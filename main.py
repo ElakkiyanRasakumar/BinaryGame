9b064c6f64aeb8168df86ca38d9dbc3feabf0036
Elakkiyan <elakkiyan2006@gmail.com>
2023-05-06 09:19:23 -0400

    Cleaned up code, and added ability to shuffle word bank

--- main.py
+++ main.py
@@ -10,13 +10,17 @@ have difficulties, that decrease the max (Base Value will be 10) ) - DO WITH GUI
 From the letters give some for vowels and constants, more for vowels than constants
 
 Some words use the same letters more than once, so account for that
-"""
 
+Check for aliasing for def want_to_play()
+
+Add scrambling of word bank
+
+Add ability to quit program
+"""
 from random import *
 from sys import exit
 import string
 import json
-
 
 
 def want_to_play():
@@ -36,6 +40,15 @@ def want_to_play():
 
 
 def roll_to_binary_to_letter_converter():
+    vowels = ["a", "e", "i", "o", "u", "y"]
+    # a = 01100001
+    # e = 01100101
+    # i = 01101001
+    # o = 01101111
+    # u = 01110101
+    # y = 01111001
+    alphabet = list(string.ascii_lowercase)  # Get the alphabet as a list in all lowercase
+
     global character_bank
     reroll = True
     while True:
@@ -45,22 +58,35 @@ def roll_to_binary_to_letter_converter():
         if len(character_bank) < 10:  # 10 for now. Check to do list to see what I have planned for this
             rolls = []  # Creating the rolls list
             og_rolls = []
+            number_of_vowels = 0
             rolls.clear()  # Clearing it for the new letter. One iteration of this loop = one letter
             og_rolls.clear()
+            letter = ""
+            for i in character_bank:
+                if i in vowels:
+                    number_of_vowels += 1
+
             if reroll:
                 input("Press enter to roll: ")
-            for _ in range(5):
-                rolls.append(randint(1, 6))  # Adding 5 numbers to list
-            for number in range(len(rolls)):  # If the roll is even it becomes a 1 and if it is odd it becomes odd
-                if rolls[number] % 2 == 0:
-                    rolls[number] = 1
-                else:
-                    rolls[number] = 0
-            for number in rolls:  # For the 5 rolls
-                binary_data += str(number)  # Add it to the first 3 letters
-            letter = chr(int(binary_data, 2))  # Converts binary to letter
 
-            alphabet = list(string.ascii_lowercase)  # Get the alphabet as a list in all lowercase
+
+            if randint(1, 10) <= 7 and number_of_vowels <= 2:
+                # 70% chance of getting a vowel
+                # 30% chance of getting a constant
+                letter = choice(vowels)
+            else:
+                for _ in range(5):
+                    rolls.append(randint(1, 6))  # Adding 5 numbers to list
+                for number in range(len(rolls)):  # If the roll is even it becomes a 1 and if it is odd it becomes odd
+                    if rolls[number] % 2 == 0:
+                        rolls[number] = 1
+                    else:
+                        rolls[number] = 0
+                for number in rolls:  # For the 5 rolls
+                    binary_data += str(number)  # Add it to the first 3 letters
+                letter = chr(int(binary_data, 2))  # Converts binary to letter
+
+
             if letter not in alphabet or letter in character_bank:  # Some of the 011XXXXX binary contain stuff
                 # beyond letters. This check if its is a letter and not a pre-existing letter in the character bank
                 reroll = False
@@ -82,45 +108,31 @@ def check_word():
     used_words = []
     file = open('Dictionary.json')
     # returns JSON as a dictionary
-    data = json.load(file)
+    word_bank = json.load(file)
     while True:
-        while True:
-            word = input("Make a word: ")
-            for i in word:
-                if i not in character_bank:  # Checks if the letters in the word are in the character bank
-                    retry = input("That word contains letter not included in the word bank. Would you like try "
-                                  "another word? (Y/N) ").title()
-                    print(character_bank)
-                    if retry == "Y":
-                        break
-                    elif retry == "N":
-                        print("Quitting Program")
-                        exit()
-                    else:
-                        continue
+        word = input("Make a word: ").lower()
+        for i in word:
+            if i not in character_bank:  # Checks if the letters in the word are in the character bank
+                print("That word contains letter not included in the word bank. Try again")
+                print(character_bank)
+                break
+        else:
+            if word in used_words:
+                print("You already used that word. Try again.") # Checks if the word has already been used
+                print(character_bank)
+
+            elif word in word_bank:  # Checks if the word is a real word
+                used_words.append(word)
+                correct_words += 1
+                print(f"Nice. Words Correct: {used_words}")
+                print(character_bank)
             else:
-                if word in data and word not in used_words:  # Checks if the word is a real word, and has not already
-                    # been used
-                    print("Nice")
-                    correct_words += 1
-                    used_words.append(word)
-                    print(f"Words: {correct_words}")
-                    print(f"Used Words: {used_words}")
+                shuffle_input = input("That was an invalid word. Try again. \nPress S to shuffle the word bank: ").lower()
+                if shuffle_input == "s":
+                    shuffle(character_bank)
                     print(character_bank)
-                else:
-                    while True:
-                        retry = input("That was an invalid word. Would you like try another word? (Y/N) ").title()  #
-                        # Again another standard user input while True loop
-                        print(character_bank)
-                        if retry == "Y":
-                            break
-                        elif retry == "N":
-                            print("Quitting Program")
-                            exit()
-                        else:
-                            continue
-            # Closing file
-            file.close()
+        # Closing file
+        file.close()
 
 
 character_bank = []
